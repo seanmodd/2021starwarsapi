9fc527e808b660078fddb71a169bc8063ba1535c
Sean <sean@vestaridefair.com>
2021-08-01 16:28:34 -0700

    awesome

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,8 @@ const [error, setError] = useState(null)
     setIsLoading(true);
     setError(null);
     try {
-    const response = await fetch('https://react-http-movies-post-default-rtdb.firebaseio.com/movies.json');
+//! URL used to be... https://swapi.dev/api/films/ !!!!!
+    const response = await fetch('https://react-http-movies-post-default-rtdb.firebaseio.com/movies.json');    
       if (!response.ok) {
         throw new Error('Something went wrong');
         }
@@ -98,7 +99,8 @@ const [error, setError] = useState(null)
     <>
       <VStack minHeight="100vh" bg={bgColor[colorMode]}>
         <Heading
-          py={20}
+          pt={20}
+          pb={10}
           align="center"
           color={textColor[colorMode]}
         >
