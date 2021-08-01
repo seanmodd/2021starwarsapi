2a222065bdf5af18facabb3af51856796c922f89
Sean <sean@vestaridefair.com>
2021-08-01 16:25:54 -0700

    maybe the most helpful lesson in all of react right here...

--- pages/index.js
+++ pages/index.js
@@ -36,15 +36,26 @@ const [error, setError] = useState(null)
         throw new Error('Something went wrong');
         }
     const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          }
-        });
-        setMovies(transformedMovies);
+    const loadedMovies = [];
+    for (const key in data) {
+      loadedMovies.push({
+        id: key,
+        title: data[key].title,
+        openingText: data[key].openingText,
+        releaseDate: data[key].releaseDate,
+      });
+    }
+    // const transformedMovies = data.results.map((movieData) => {
+    //       return {
+    //         id: movieData.episode_id,
+    //         title: movieData.title,
+    //         openingText: movieData.opening_crawl,
+    //         releaseDate: movieData.release_date,
+    //       }
+    //     });
+    
+        // setMovies(transformedMovies);
+        setMovies(loadedMovies);
         setIsLoading(false);
       } 
     catch (error) {
